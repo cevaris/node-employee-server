da565f73cfd9c929546339ed6770093a32d9bb30
Adam Cardenas <cevaris@gmail.com>
2019-10-06 13:28:00 -0600

    added simple api

--- index.ts
+++ index.ts
@@ -1,11 +1,44 @@
-import express from 'express';
+import express, { response } from 'express';
+import { isNumber } from 'util';
+
+interface Employee {
+    name: string,
+    salary: string
+}
+
+const store: Employee[] = new Array();
 
 var app = express();
+app.use(express.json());
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
+app.post('/employees/create', function (req, res) {
+    const employee = req.body;
+    if (employee) {
+        store.push(employee);
+        res.json(employee);
+    } else {
+        console.error(`failed to create employee ${employee}`)
+    }
+});
+
+app.get('/employees/:id', function (req, res) {
+    console.log(req.params);
+    const index = parseInt(req.params.id);
+    console.log(index);
+    if (isNumber(index) && index < store.length) {
+        res.json(store[index]);
+    } else {
+        res.json({ 'error': `no such employee ${index}` });
+    }
+});
+
+app.get('/employees', function (req, res) {
+    res.json(store);
+});
 
 const PORT = process.env.PORT || '3000';
 app.listen(PORT, () => {
